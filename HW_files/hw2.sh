c4d3dcab9c5e073d64fe15d92d068c3a281d3118
madrharris <madrharris22@gmail.com>
2022-02-15 08:04:55 -0800

    Adding the hw sh file, plus some extra git told me to add and commit

--- HW_files/hw2.sh
+++ HW_files/hw2.sh
@@ -9,25 +9,24 @@
 ## --> 1. Make a new repository via github
 ### be on desktop
 
-mkdir Shared_dir/
-cd Shared_dir/
-git init
-git checkout -b main
+
 
 
 
 ## can also make a repo via github and clone it to the computer
-## git clone https://github.com/madrharris/Bio792_Shared
+git clone https://github.com/madrharris/SharedRepo
+cd SharedRepo/
+git pull origin master --allow-unrelated-histories
 touch README.md
 git add README.md
 git commit -m "adding README"
-mkdir notes/
-touch testfile.txt
-mv testfile.txt notes/
-git add notes/testfile.txt
-git commit -m "adding file and directory"
+#mkdir notes/
+#touch testfile.txt
+#mv testfile.txt notes/
+#git add notes/testfile.txt
+#git commit -m "adding file and directory"
 git push
 
-git pull origin master --allow-unrelated-histories
-git status
 
+
+####		gives a few errors?? but works all the same		###
