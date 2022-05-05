124c2867766500094ad9cd308b9e400beae12ac6
madrharris <madrharris22@gmail.com>
2022-05-05 13:50:04 -0700

    pseudocode & separate files code

---
+++ PseudocodeMadison.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+## create an empty list
+### Open the file
+	### read through each line
+	### Add each site to the list
+## close the file
+
+### turn the list into a set -- will shorten it ton only the unique numbers
+
+## create a for loop
+## for each item in the set - 
+	## create an output file with the name of the item in the set
+	## open the input file
+	## read through each line 
+		# ask if the site name matches the item in the list
+		# if it does print the line to the file
+	#close the input file
+	#close the output file
+
+
+
